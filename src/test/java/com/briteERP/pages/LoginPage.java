7eae8a1e4f162ecb17cb8eb15e0396ccc6db3795
betlmath <betlmath@gmail.com>
2019-08-14 12:31:43 -0500

    update

--- src/test/java/com/briteERP/pages/LoginPage.java
+++ src/test/java/com/briteERP/pages/LoginPage.java
@@ -1,4 +1,49 @@
 package com.briteERP.pages;
 
-public class LoginPage {
+
+import com.briteERP.utils.BriteUtils;
+import com.briteERP.utils.PageBase;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends PageBase {
+
+    @FindBy(id="login")
+    public WebElement userNameLocator;
+
+    @FindBy(id="password")
+    public WebElement passwordLocator;
+
+    @FindBy(css = "button[class='btn btn-primary']")
+    public WebElement logInButton;
+
+    @FindBy(linkText = "Don't have an account?")
+    public WebElement noAccountLocator;
+
+    @FindBy(linkText = "Reset Password")
+    public WebElement resetPasswordLocator;
+
+    @FindBy(css = "div[class='form-group field-password']+p")
+    public WebElement errorMessageLocator;
+
+    @FindBy(css = "div[class='clearfix oe_login_buttons']>button")
+    public  WebElement confirmButtonLocator;
+
+    public void login(String username, String password){
+        userNameLocator.sendKeys(username);
+        passwordLocator.sendKeys(password);
+        logInButton.click();
+        BriteUtils.waitForPageToLoad(5);
+    }
+
+    public String getErrorMessage(){
+        return errorMessageLocator.getText();
+    }
+
+    public void resetPassword(String newPassword){
+        resetPasswordLocator.click();
+        userNameLocator.sendKeys(newPassword);
+        confirmButtonLocator.click();
+    }
+
 }
--- src/test/java/com/briteERP/tests/LoginPageTests.java
+++ src/test/java/com/briteERP/tests/LoginPageTests.java
@@ -1,4 +1,26 @@
 package com.briteERP.tests;
 
-public class LoginPageTests {
+import com.briteERP.utils.ConfigurationReader;
+import com.briteERP.utils.TestBase;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginPageTests extends TestBase {
+
+    @Test(description = "positive login test")
+    public void loginTest1(){
+        extentLogger = report.createTest("Login as event manager");
+        pages.loginPage().login(ConfigurationReader.getProperty("username"),
+            ConfigurationReader.getProperty("password"));
+    }
+
+    @Test(description = "negative login test")
+    public void loginTest2(){
+        extentLogger = report.createTest("Negative login test");
+        extentLogger.info("Login with wrong username and wrong password");
+        pages.loginPage().login("username", "invalidPassword");
+        String msg = pages.loginPage().getErrorMessage();
+        Assert.assertEquals(msg,"Wrong login/password");
+        extentLogger.pass("Verified that Message present: " + pages.loginPage().getErrorMessage());
+    }
 }
